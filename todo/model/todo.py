a9ba3f1c344981124ecf7c2ba40d0892dff46d71
Mariaisabel2 <mquinteroisabel32@gmail.com>
2024-08-26 13:03:59 -0500

    init code fix

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,30 +1,34 @@
 # TODO: Add code here
 
 class Todo:
-    def __init__(self, code_id: int, title: str, description: str, completed: bool, tags: list[str]):
+    def __init__(self, code_id: int, title: str, description: str):
         self.code_id = code_id
         self.title = title
         self.description = description
-        self.completed = False
-        self.tags = []
+        self.completed: bool = False
+        self.tags: list[str] = []
 
     def mark_completed(self):
         self.completed = True
 
     def add_tag(self, tag: str):
         if tag not in self.tags:
-            self.tags.append(tag)
+            self.tags.append(f"{tag}")
+
     def __str__(self) -> str:
         return f"{self.code_id} - {self.title}"
+
 
 class TodoBook:
     def __init__(self):
         self.todos = {}
+
     def add_todo(self, title: str, description: str) -> int:
         element_id = len(self.todos) + 1
-        new_todo = Todo(code_id=element_id, title=title, description=description, completed= False, tags=[])
+        new_todo = Todo(code_id=element_id, title=title, description=description)
         self.todos[element_id] = new_todo
         return element_id
+
     def pending_todos(self) -> list:
         pending_list = [todo for todo in self.todos.values() if not todo.completed]
         return pending_list
@@ -32,6 +36,7 @@ class TodoBook:
     def completed_todos(self) -> list:
         completed_list = [todo for todo in self.todos.values() if todo.completed]
         return completed_list
+
     def tags_todo_count(self) -> dict:
         tag_count = {}
         for todo in self.todos.values():
